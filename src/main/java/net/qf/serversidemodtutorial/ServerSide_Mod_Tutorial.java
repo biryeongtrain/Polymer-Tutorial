447e7c2473c4321e0af8a7a00da8a65ca78d367f
biryeongtrain <83863154+biryeongtrain@users.noreply.github.com>
2023-10-21 20:37:44 +0900

    init

---
+++ src/main/java/net/qf/serversidemodtutorial/ServerSide_Mod_Tutorial.java
@@ -0,0 +1,10 @@
+package net.qf.serversidemodtutorial;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ServerSide_Mod_Tutorial implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
